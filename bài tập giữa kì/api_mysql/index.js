04f875a1bb5b7a367b2548336580b13993f9677e
LeeTonion <tttttttt21102005@gmail.com>
2024-11-10 21:06:39 +0700

    up

--- bài tập giữa kì/api_mysql/index.js
+++ bài tập giữa kì/api_mysql/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const todoRoutes = require('./src/routers/todoRoutes');
-const userRoutes = require('./src/routers/userRoutes');
+
 const completeRoutes = require('./src/routers/completeRoutes');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json()); 
 app.use('/api', todoRoutes); 
-app.use('/user',userRoutes)
+
 app.use('/apis',completeRoutes)
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
